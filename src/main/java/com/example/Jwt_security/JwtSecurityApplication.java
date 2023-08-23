1574cdffd320a7f39dba0a7909c262bb4614a087
shmsacr <shmsacr@gmail.com>
2023-08-23 17:16:16 +0300

    Initial commit

---
+++ src/main/java/com/example/Jwt_security/JwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jwt_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityApplication.class, args);
+	}
+
+}
