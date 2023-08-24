62bef4eaa152dec5cd50ef395834e6a830489b00
shmsacr <shmsacr@gmail.com>
2023-08-24 20:20:52 +0300

    feat: add token valid service and expiration

--- src/main/java/com/example/Jwt_security/config/JwtService.java
+++ src/main/java/com/example/Jwt_security/config/JwtService.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "CD62E1E2F2BAD84FE427343B3FCEA";
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
     public String extractUsername(String token) {
         return extractClaim(token,Claims::getSubject);
     }
@@ -29,12 +29,27 @@ public class JwtService {
     public String generateToken(UserDetails userDetails){
         return generateToken(new HashMap<>(),userDetails);
     }
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
 
     public String generateToken(
             Map<String, Objects> extraClaims,
             UserDetails userDetails
     ){
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername()).setIssuedAt(new Date(System.currentTimeMillis()))
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() +1000*60 *24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
     }
